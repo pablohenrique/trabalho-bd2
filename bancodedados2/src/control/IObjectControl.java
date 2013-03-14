2c045fc73b24811f9b8d31129c31680ec1535993
Yuri Campos <yuricampos@Yuris-iMac.local>
2013-03-14 18:56:26 -0300

    Atualizacoes na interface de controle

--- bancodedados2/src/control/IObjectControl.java
+++ bancodedados2/src/control/IObjectControl.java
@@ -4,10 +4,53 @@
  */
 package control;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author yuricampos
  */
 public interface IObjectControl {
+    
+        /**
+     * Usado para inserir um objeto no banco de dados
+     * @param Objeto
+     * @return void
+     */
+    public void post(Object input);
+    
+        /**
+     * Usado para atualizar um objeto no banco de dados
+     * @param Objeto
+     * @return void
+     */
+    public void update(Object input);
+    
+     /**
+     * Usado para recuperar um objeto do banco de dados
+     * @param int(id)
+     * @return Object
+     */
+    public Object getById(int input);
+    
+
+    
+     /**
+     * Usado para recuperar todos os objetos de um banco de dados
+     * @param Object
+     * @return ArrayList<Object>
+     */
+    public ArrayList<Object> getAll(Object input);
+    
+     /**
+     * Usado para recuperar todos os objetos de um banco de dados que possuam
+     * o nome informado (busca)
+     * @param String(nome)
+     * @return ArrayList<Object>
+     */
+    public ArrayList<Object> getSearchByName(String input);
+    
+    
+    
     
 }
