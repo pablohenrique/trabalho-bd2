6801b8728fd60688e0117dd181968b44ea44520e
Pablo Henrique <pablohenrique@linuxmint.(none)>
2013-03-15 20:15:47 -0300

    Conexao modificada

--- bancodedados2/src/DAO/Conexao.java
+++ bancodedados2/src/DAO/Conexao.java
@@ -4,16 +4,35 @@
  */
 package DAO;
 
-import java.sql.Connection;
+import java.sql.*;
 
 /**
  *
  * @author pablohenrique
  */
 public class Conexao {
-    private Conexao(){};
+    private static final String url = "jdbc:postgresql://177.71.252.132:5432/trabalho_bd2?searchpath=cia";
+    private static final String user = "postgres";
+    private static final String pass = "25081992";
+    private static Connection connection;
     
-    public static Connection getConnection(){
-        return null;
+    private Conexao(){
+        try {
+            Class.forName( "org.postgresql.Driver" );
+            connection = DriverManager.getConnection(url, user, pass);
+        } 
+        catch ( ClassNotFoundException cnfex ) {
+            System.err.println("Failed to load JDBC/ODBC driver. erro" + cnfex.toString() );
+        }
+        catch ( SQLException sqlex ) {
+            System.err.println( "Unable to connect" + sqlex.toString() );
+        }
+    }
+    
+    public static Connection getConexao() throws SQLException{
+        if(connection != null)
+            return connection;
+        else
+            throw new SQLException("Conexao nao foi criada");
     }
 }
