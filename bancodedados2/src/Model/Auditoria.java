8d27ef2fcc4f4e75cd76c7534016accddeaa4f55
Yuri Campos <yuricampos@me.com>
2013-03-16 18:21:17 -0300

    Criacao model auditoria

---
+++ bancodedados2/src/Model/Auditoria.java
@@ -0,0 +1,92 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author yuricampos
+ */
+public class Auditoria {
+    private String gerssn;
+    private String essn;
+    private float antigosalario;
+    private float novosalario;
+    private Date datamodificacao;
+
+    /**
+     * @return the gerssn
+     */
+    public String getGerssn() {
+        return gerssn;
+    }
+
+    /**
+     * @param gerssn the gerssn to set
+     */
+    public void setGerssn(String gerssn) {
+        this.gerssn = gerssn;
+    }
+
+    /**
+     * @return the essn
+     */
+    public String getEssn() {
+        return essn;
+    }
+
+    /**
+     * @param essn the essn to set
+     */
+    public void setEssn(String essn) {
+        this.essn = essn;
+    }
+
+    /**
+     * @return the antigosalario
+     */
+    public float getAntigosalario() {
+        return antigosalario;
+    }
+
+    /**
+     * @param antigosalario the antigosalario to set
+     */
+    public void setAntigosalario(float antigosalario) {
+        this.antigosalario = antigosalario;
+    }
+
+    /**
+     * @return the novosalario
+     */
+    public float getNovosalario() {
+        return novosalario;
+    }
+
+    /**
+     * @param novosalario the novosalario to set
+     */
+    public void setNovosalario(float novosalario) {
+        this.novosalario = novosalario;
+    }
+
+    /**
+     * @return the datamodificacao
+     */
+    public Date getDatamodificacao() {
+        return datamodificacao;
+    }
+
+    /**
+     * @param datamodificacao the datamodificacao to set
+     */
+    public void setDatamodificacao(Date datamodificacao) {
+        this.datamodificacao = datamodificacao;
+    }
+    
+    
+    
+}
