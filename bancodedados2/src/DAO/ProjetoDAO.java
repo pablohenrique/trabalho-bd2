182951908a308068c55bebb0cd61ad90cd1525b9
Pablo Henrique <pablohenrique@linuxmint.(none)>
2013-04-14 03:25:52 -0300

    projeto

--- bancodedados2/src/DAO/ProjetoDAO.java
+++ bancodedados2/src/DAO/ProjetoDAO.java
@@ -28,10 +28,10 @@ public class ProjetoDAO implements IObjectDAO{
     private final String SQL_POST = "INSERT INTO cia.projeto(pjnome, plocalizacao, dnum) VALUES(?,?,?);";
     private final String SQL_UPDATE = "UPDATE cia.projeto SET pjnome = ?, plocalizacao = ?, dnum = ? WHERE pnumero = ?;";
     private final String SQL_DELETE = "DELETE FROM cia.projeto WHERE pnumero = ?;";
-    private final String SQL_GET = "SELECT DISTINCT(p.pnumero)," + BEFORECOND + " WHERE p.pnumero = ? AND t.pjnumero = p.pnumero AND d.gerssn = e.ssn AND p.dnum = d.numero;";
-    private final String SQL_READ = "SELECT DISTINCT(p.pnumero)," + BEFORECOND + " WHERE p.pjnome = ? AND t.pjnumero = p.pnumero AND d.gerssn = e.ssn AND p.dnum = d.numero;";
+    private final String SQL_GET = "SELECT DISTINCT(p.pnumero)," + SELECT_DEP + SELECT_EMP + " FROM cia.empregado AS e,  cia.projeto AS p,  cia.departamento AS d WHERE p.pnumero = ? AND d.gerssn = e.ssn AND p.dnum = d.numero;";
+    private final String SQL_READ = "SELECT DISTINCT(p.pnumero)," + SELECT_DEP + SELECT_EMP + " FROM cia.empregado AS e,  cia.projeto AS p,  cia.departamento AS d WHERE p.pjnome LIKE UPPER(?) AND d.gerssn = e.ssn AND p.dnum = d.numero;";
     private final String SQL_GETALL_JOIN = "SELECT p.pnumero AS p_numero, p.pjnome AS p_nome, p.plocalizacao AS p_localizacao,d.numero AS d_numero, d.nome AS d_nome, d.gerssn AS d_gerssn, d.gerdatainicio AS d_dataInicio,e.ssn AS e_ssn, e.nome AS e_nome, cia.sexo(e.sexo) AS e_sexo, e.endereco AS e_endereco, e.salario AS e_salario, e.datanasc AS e_datanasc, e.dno AS e_dno, e.superssn AS e_superssn, e.senha AS e_senha,t.essn as t_essn, t.pjnumero AS t_numero, t.horas AS t_horas FROM (((cia.projeto as p LEFT OUTER JOIN cia.departamento AS d ON d.numero = p.dnum) LEFT OUTER JOIN cia.empregado AS e ON d.gerssn = e.ssn) LEFT OUTER JOIN cia.trabalha_em AS t ON t.essn = e.ssn);";
-    private final String SQL_GETALLDEPNOME = "SELECT " + SELECT_PRJETO + SELECT_DEP + SELECT_EMP + "  FROM cia.empregado AS e,  cia.projeto AS p,  cia.departamento AS d WHERE UPPER(p.pjnome) LIKE UPPER('%'|| ? ||'%')  AND d.gerssn = e.ssn AND p.dnum = d.numero;";
+    private final String SQL_GETALLDEPNOME = "SELECT " + SELECT_PRJETO + SELECT_DEP + SELECT_EMP + "  FROM cia.empregado AS e,  cia.projeto AS p,  cia.departamento AS d WHERE UPPER(d.nome) LIKE UPPER(?)  AND d.gerssn = e.ssn AND p.dnum = d.numero;";
     private final String SQL_GETALLDEPNUMERO = "SELECT DISTINCT(p.pnumero)," + BEFORECOND + " WHERE d.numero = ? AND t.pjnumero = p.pnumero AND d.gerssn = e.ssn AND p.dnum = d.numero;";
     private final String SQL_GETALLEMP = "SELECT " + BEFORECOND + " WHERE e.ssn = t.essn AND t.pjnumero = p.pnumero AND p.dnum = d.numero AND e.ssn = ? ORDER BY t.horas ASC;";
     
@@ -40,11 +40,10 @@ public class ProjetoDAO implements IObjectDAO{
            
     private Object criarObjetoTemplate(){
         try {
-            String column = "p_";
             Projeto output = new Projeto();
-            output.setNumero(this.rs.getInt(column+"numero"));
-            output.setNome(this.rs.getString(column+"nome"));
-            output.setLocalizacao(this.rs.getString(column+"localizacao"));
+            output.setNumero(this.rs.getInt("p_numero"));
+            output.setNome(this.rs.getString("p_nome"));
+            output.setLocalizacao(this.rs.getString("p_localizacao"));
             //output.setHoras(this.rs.getFloat("t_horas"));
             
             //EmpregadoDAO empdao = (EmpregadoDAO) FactoryDAO.getFactory("Empregado");
@@ -147,14 +146,15 @@ public class ProjetoDAO implements IObjectDAO{
     @Override
     public Object read(Object input) {
         try {
+            String aux = "%" + (String) input + "%";
             this.ps = Conexao.getInstance().getConexao().prepareStatement(SQL_READ);
-            this.ps.setString(1,(String) input);
+            this.ps.setString(1,aux);
             this.rs = this.ps.executeQuery();
             
-            if(!this.rs.next())
-                throw new Exception("Departamento nao encontrado.");
-            
-            return this.criarObjetoTemplate();
+            if(this.rs.isLast())
+                return this.criarObjetoTemplate();
+            else
+                return this.buscarVariosObjetosTemplate();
             
         } catch (Exception e) {
             System.err.println("Erro ao buscar [READ] o objeto:  " + e.toString() );
