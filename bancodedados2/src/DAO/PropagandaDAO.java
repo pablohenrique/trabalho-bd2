dd77632e1fef3e550c87f7e679c36127faa721d5
Yuri Campos <yuricampos@me.com>
2013-04-17 00:43:14 -0300

    correcao no update

--- bancodedados2/src/DAO/PropagandaDAO.java
+++ bancodedados2/src/DAO/PropagandaDAO.java
@@ -22,7 +22,7 @@ public class PropagandaDAO implements IObjectDAO{
 "p.pnumero as p_pnumero, p.pjnome as p_pjnome, p.plocalizacao as p_localizacao, p.dnum as p_dnum";
     
     private final String SQL_POST = "INSERT INTO cia.propaganda(projeto, dataInicio, dataFinal, agencia, tarifa) VALUES(?,?,?,?,?);";
-    private final String SQL_UPDATE = "UPDATE cia.propaganda AS pp SET pp.projeto = ? AND pp.dataInicio = ? AND pp.dataFinal = ? AND pp.agencia = ? AND pp.tarifa = ? WHERE pp.id = ?;";
+    private final String SQL_UPDATE = "UPDATE cia.propaganda SET dataInicio = ?, dataFinal = ?,agencia = ?, tarifa = ? WHERE id = ?;";
     private final String SQL_GET = BEFORECOND + " FROM cia.propaganda as pp, cia.projeto as p WHERE pp.id = ? AND pp.projeto = p.pnumero;";
     private final String SQL_READ = BEFORECOND + " FROM cia.propaganda as pp, cia.projeto as p WHERE pp.projeto = ? AND pp.projeto = p.pnumero;";
     private final String SQL_GETALL = BEFORECOND + " FROM cia.propaganda as pp, cia.projeto as p WHERE pp.projeto = p.pnumero;";
@@ -78,12 +78,11 @@ public class PropagandaDAO implements IObjectDAO{
         this.ps = Conexao.getInstance().getConexao().prepareStatement(SQL_UPDATE);
 
         Propaganda pro = (Propaganda) input;
-        this.ps.setInt(1, pro.getProjeto().getNumero());
-        this.ps.setDate(2, pro.getDataInicio());
-        this.ps.setDate(3, pro.getDataFinal());
-        this.ps.setString(4, pro.getAgencia());
-        this.ps.setFloat(5, pro.getTarifa());
-        this.ps.setInt(6, pro.getNumero());
+        this.ps.setDate(1, pro.getDataInicio());
+        this.ps.setDate(2, pro.getDataFinal());
+        this.ps.setString(3, pro.getAgencia());
+        this.ps.setFloat(4, pro.getTarifa());
+        this.ps.setInt(5, pro.getNumero());
 
         this.rs = this.ps.executeQuery();
         if(!this.rs.next())
