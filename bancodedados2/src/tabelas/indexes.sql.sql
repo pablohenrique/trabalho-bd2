af4bf1a7b4fa4502ec4c5d382631509ad002724b
Pablo Henrique <pablohenrique@linuxmint.(none)>
2013-04-19 16:25:15 -0300

    indexes

---
+++ bancodedados2/src/tabelas/indexes.sql.sql
@@ -0,0 +1,12 @@
+--
+-- INDEXES
+--
+
+CREATE INDEX index_empregado_salario
+    ON empregado(salario);
+
+CREATE INDEX index_salario_departamento
+    ON empregado(dno);
+
+CREATE INDEX index_departamento_gerente
+    ON departamento(gerssn);
